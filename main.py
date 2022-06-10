2f4c5acb43ef8066eced0b835fd37c766eeeaeaf
Berenyi Gergely <gergelyberenyi@gmail.com>
2022-06-10 21:17:33 +0200

    second test3

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-def cucc():
-    return "csirkefarhat"
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 
-def stg():
-    from webdriver_manager.chrome import ChromeDriverManager
-    from selenium import webdriver
 
-    browser = webdriver.Chrome(ChromeDriverManager().install())
-    URL = "http://localhost:1667/"
-    browser.get(URL)
+class TestConduit:
+    def setup(self):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        URL = "http://localhost:1667/"
+        self.browser.get(URL)
 
-    sign_up_btn = browser.find_element_by_xpath('.//a[@href="#/register"]')
-    sign_up_btn.click()
-    thing = browser.find_element_by_xpath('.//a[@href="#/login"]')
-    if thing != '':
-        return "yeah"
+    def test_registration(self):
+        sign_in_btn = self.browser.find_element_by_xpath('.//a[@href="#/register"]')
+        sign_in_btn.click()
+        username = self.browser.find_element_by_xpath('.//input[@placeholder="Username"]')
+        username.send_keys('Béla')
+        email = self.browser.find_element_by_xpath('.//input[@placeholder="Email"]')
+        email.send_keys('bela1@bela.com')
+        pwd = self.browser.find_element_by_xpath('.//input[@placeholder="Password"]')
+        pwd.send_keys('A123456a')
+        sign_up_btn = self.browser.find_element_by_xpath('//button[@class="btn btn-lg btn-primary pull-xs-right"]')
+        sign_up_btn.click()
--- test_main.py
+++ test_main.py
@@ -1,10 +1,4 @@
 import pytest
 from main import *
 
-
-def test_cucc1():
-    assert cucc() == 'csirkefarhat'
-
-def test_cucc2():
-    assert stg() == 'yeah'
-
+testem = TestConduit()
